3f494c0d736ae6b7eb5241457c05e2ebf0a5883c
Bonsa-Dereje <bonsa.dereje.dir@gmail.com>
2026-04-27 07:49:01 +0300

    add GET endpoints

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const http = require("http");
+const fs = require("fs");
 
 const PORT = 3000;
+const FILE = "./movies.json";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "API is running" }));
+  if (req.method === "GET" && req.url === "/movies") {
+    fs.readFile(FILE, "utf8", (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end("Error reading file");
+      }
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(data);
+    });
+  }
+
+  else if (req.method === "GET" && req.url.startsWith("/movies/")) {
+    const id = req.url.split("/")[2];
+
+    fs.readFile(FILE, "utf8", (err, data) => {
+      const movies = JSON.parse(data);
+      const movie = movies.find(m => m.id == id);
+
+      if (!movie) {
+        res.writeHead(404);
+        return res.end("Movie not found");
+      }
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(movie));
+    });
+  }
+
+  else {
+    res.writeHead(404);
+    res.end("Route not found");
+  }
 });
 
 server.listen(PORT, () => {
